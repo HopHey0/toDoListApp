4d609c60a9aae8c4066c8d27f4fcee542d25ae99
Sitnov_MV <sitnov.m.v@edu.mirea.ru>
2025-12-13 00:03:05 +0300

    Nav, testnav, serialization and viemodel dependencies are added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.navigation.compose)
+    androidTestImplementation(libs.androidx.navigation.testing)
+    implementation(libs.kotlinx.serialization.json)
 }
