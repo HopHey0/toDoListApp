a375b304d89a78ae6026245f60cf5408b54afe24
HopHey0 <mishasitnov@yandex.ru>
2025-12-13 19:08:34 +0300

    ToggleTodoUseCase updated, invoke logic added

--- app/src/main/java/com/example/pract_7_8/domain/usecase/ToggleTodoUseCase.kt
+++ app/src/main/java/com/example/pract_7_8/domain/usecase/ToggleTodoUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.pract_7_8.domain.usecase
 
+import com.example.pract_7_8.domain.model.TodoItem
 import com.example.pract_7_8.domain.repository.TodoRepository
 
 class ToggleTodoUseCase(private val repository: TodoRepository) {
-
+    suspend operator fun invoke(id: Int): Unit = repository.toggleTodo(id)
 }
