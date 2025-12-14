93687da8a80bd1a83c69ee63a38e222ec6013e8f
HopHey0 <mishasitnov@yandex.ru>
2025-12-14 03:00:11 +0300

    Added a test that checks checkboxes state and changing data

--- app/src/androidTest/java/com/example/pract_7_8/CheckBoxTest.kt
+++ app/src/androidTest/java/com/example/pract_7_8/CheckBoxTest.kt
@@ -3,9 +3,12 @@ package com.example.pract_7_8
 import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import androidx.test.platform.app.InstrumentationRegistry
@@ -16,22 +19,24 @@ import com.example.pract_7_8.domain.usecase.ToggleTodoUseCase
 import com.example.pract_7_8.navigation.AppNavHost
 import com.example.pract_7_8.presentation.viewmodel.TodolistViewModel
 import com.example.pract_7_8.ui.theme.Pract_7_8Theme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-
-class TodosUITest {
+class CheckBoxTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
-    @Test
-    fun checkIfTodosOnScreen(){
+    private lateinit var viewModel: TodolistViewModel
+
+    @Before
+    fun initActivity(){
         val dataSource = TodoJsonDataSource(context)
         val repository = ToDoRepositoryImpl(dataSource)
         val getTodosUseCase = GetTodosUseCase(repository)
         val toggleTodoUseCase = ToggleTodoUseCase(repository)
-        val viewModel = TodolistViewModel(
+        viewModel = TodolistViewModel(
             getTodosUseCase = getTodosUseCase,
             toggleTodoUseCase = toggleTodoUseCase
         )
@@ -41,19 +46,41 @@ class TodosUITest {
                 AppNavHost(viewModel, navController, Modifier)
             }
         }
-
-        /*
-        Содержимое первых трех задач из JSON-файла
-        [
-          {"id": 1,"title": "Купить молоко","description": "2 литра, обезжиренное","isCompleted": false},
-          {"id": 2,"title": "Позвонить маме","description": "Спросить про выходные","isCompleted": true},
-          {"id": 3,"title": "Сделать ДЗ по Android","description": "Clean Architecture + Compose","isCompleted": false}
-        ]
-         */
-
-        composeTestRule.onNodeWithText("Купить молоко").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Спросить про выходные").assertIsDisplayed()
-        composeTestRule.onNodeWithText("Сделать ДЗ по Android").assertIsDisplayed()
     }
 
+    @Test
+    fun checkIfBoxIsSelectedAfterClick(){
+        composeTestRule.onNodeWithTag("RowCheckBox3")
+            .assertIsOff()
+            .performClick()
+            .assertIsOn()
+
+        composeTestRule.onNodeWithTag("RowCheckBox4")
+            .assertIsOn()
+            .performClick()
+            .performClick()
+            .assertIsOn()
+
+        composeTestRule.onNodeWithTag("RowCheckBox10")
+            .performClick()
+            .performClick()
+            .performClick()
+            .assertIsOff()
+
+    }
+
+    @Test
+    fun checkIfDataChangedAfterClick() {
+        val sample1Before = viewModel.todos.find { it.id == 3}?.isCompleted
+        composeTestRule.onNodeWithTag("RowCheckBox3").performClick()
+        val sample1After = viewModel.todos.find { it.id == 3}?.isCompleted
+
+        val sample2Before = viewModel.todos.find { it.id == 7}?.isCompleted
+        composeTestRule.onNodeWithTag("RowCheckBox7").performClick()
+        composeTestRule.onNodeWithTag("RowCheckBox7").performClick()
+        val sample2After = viewModel.todos.find { it.id == 7}?.isCompleted
+
+        assert(sample1Before != sample1After)
+        assert(sample2Before == sample2After)
+    }
 }
--- app/src/main/java/com/example/pract_7_8/presentation/ui/component/ToDoRow.kt
+++ app/src/main/java/com/example/pract_7_8/presentation/ui/component/ToDoRow.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +36,7 @@ fun ToDoRow(
     ){
         Checkbox(
             modifier = Modifier.padding(horizontal = 25.dp)
+                .testTag("RowCheckBox${item.id}")
                 .weight(1f),
             checked = item.isCompleted,
             onCheckedChange = {onChecked()}
